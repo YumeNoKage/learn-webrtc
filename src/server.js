eca11d29322bb5548a1bc80ea47efedf136c6e13
erico_pj <erico.pratama@logique.co.id>
2024-01-03 21:42:16 +0700

    im broke the code

--- src/server.js
+++ src/server.js
@@ -15,15 +15,21 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'))
 });
 
+app.get('/count', (req, res) => {
+	const count = io.engine.clientsCount;
+	res.status(200)
+	res.send({count})
+})
+
 io.on('connection', (socket) => {
-	console.log('User Connected')
+	console.log('User connected:', socket.id)
 
 	// handle WebRTC signaling events
 	// Handle the creation of a new session
 	socket.on('createSession', () => {
 		const sessionId = generateSessionId();
 		socket.join(sessionId);
-		activeSessions.set(sessionId, [socket.id]);
+		activeSessions.set(sessionId, { users: [socket.id], connections: new Map() })
 
 		// Send the session ID back to the client
 		socket.emit('sessionCreated', { sessionId });
@@ -31,16 +37,21 @@ io.on('connection', (socket) => {
 
 	// Handle joining an existing session
 	socket.on('joinSession', ({ sessionId }) => {
-		if (activeSessions.has(sessionId)) {
-			socket.join(sessionId);
-			activeSessions.get(sessionId).push(socket.id);
+		// Code to handle user joining a room
+		socket.join(sessionId)
+		activeSessions.set(sessionId, { users: [socket.id], connections: new Map() })
+		socket.emit('userJoined', activeSessions[sessionId])
 
-			// Notify all users in the session about the new participant
-			io.to(sessionId).emit('userJoined', { sessionId, userId: socket.id });
-		} else {
-			// Handle invalid session ID
-			socket.emit('invalidSession');
-		}
+		// Notify all users in the session about the new participant
+		// io.to(sessionId).emit('userJoined', { sessionId, userId: socket.id });
+		// if (activeSessions.has(sessionId)) {
+		// 	socket.join(sessionId);
+		// 	activeSessions.get(sessionId).push(socket.id);
+
+		// } else {
+		// 	// Handle invalid session ID
+		// 	socket.emit('invalidSession');
+		// }
 	});
 
 	// Handle messages within a session
@@ -49,33 +60,59 @@ io.on('connection', (socket) => {
     io.to(sessionId).emit('message', { userId: socket.id, message });
   });
 
-	socket.on('offer', (offer) => {
-		socket.broadcast.emit('offer', offer)
-	})
+	socket.on('offer', (data) => {
+    // Broadcast the offer to other clients in the same room
+		socket.to(data.room).emit('offer', { offer: data.offer, sender: socket.id });
+  });
 
-	socket.on('answer', (answer) => {
-		socket.broadcast.emit('answer', answer)
-	})
+  socket.on('answer', (data) => {
+    // Broadcast the answer to other clients in the same room
+		socket.to(data.room).emit('answer', { answer: data.answer, sender: socket.id });
+  });
 
-	socket.on('ice-candidate', (candidate) => {
-		// console.log(candidate)
-		socket.broadcast.emit('ice-candidate', candidate)
-	})
+  socket.on('ice-candidate', (data) => {
+    // Broadcast the ICE candidate to other clients in the same room
+		socket.to(data.room).emit('ice-candidate', { candidate: data.candidate, sender: socket.id });
+  });
 
 	// Handle disconnecting
   socket.on('disconnect', () => {
     console.log('User disconnected');
 
     // Remove the user from the active session
-    activeSessions.forEach((userList, sessionId) => {
-      const index = userList.indexOf(socket.id);
-      if (index !== -1) {
-        userList.splice(index, 1);
-        io.to(sessionId).emit('userLeft', { sessionId, userId: socket.id });
+    // activeSessions.forEach((userList, sessionId) => {
+    //   const index = userList.indexOf(socket.id);
+    //   if (index !== -1) {
+    //     userList.splice(index, 1);
+    //     io.to(sessionId).emit('userLeft', { sessionId, userId: socket.id });
 
-        // If no users left in the session, delete it
-        if (userList.length === 0) {
-          activeSessions.delete(sessionId);
+    //     // If no users left in the session, delete it
+    //     if (userList.length === 0) {
+    //       activeSessions.delete(sessionId);
+    //     }
+    //   }
+    // });
+		activeSessions.forEach((roomData, room) => {
+      const { users, connections } = roomData;
+      const userIndex = users.indexOf(socket.id);
+
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+
+        // Close connections associated with the disconnected user
+        const userConnections = connections.get(socket.id);
+        if (userConnections) {
+          userConnections.forEach((connection, otherUser) => {
+            io.to(otherUser).emit('userLeft', socket.id);
+            connection.close();
+          });
+        }
+
+        // If no users left in the room, delete the room
+        if (users.length === 0) {
+          activeSessions.delete(room);
+        } else {
+          socket.to(room).emit('userLeft', socket.id);
         }
       }
     });
